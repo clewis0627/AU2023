0a6fa72a1fcb3a82a5bfd527491105b6fba7c0b6
Cameron Lewis <34168521+clewis0627@users.noreply.github.com>
2023-04-26 18:48:13 -0500

    Update Random_Password.py

--- Random_Password.py
+++ Random_Password.py
@@ -35,10 +35,14 @@ num5 = random.randint(0,9)
 num6 = random.randint(0,9)
 num7 = random.randint(0,9)
 
+# List of available Special Characters for use in passwords according to IBM
+# Did not include characters that were unavailable to use as the first character
+specialCharacterOptions = [33, 40, 41, 63, 91, 93, 64, 35, 36, 37, 38, 94, 42]
+
 # Randomly chooses three special characters
-spcChar1 = chr(random.randint(33,38)) # OR random.randint(63,64))
-spcChar2 = chr(random.randint(33,38)) # OR random.randint(63,64))
-spcChar3 = chr(random.randint(33,38)) # OR random.randint(63,64))
+spcChar1 = chr(specialCharacterOptions[random.randint(0,12)])
+spcChar2 = chr(specialCharacterOptions[random.randint(0,12)])
+spcChar3 = chr(specialCharacterOptions[random.randint(0,12)])
 
 # Combines all characters into a string to be later randomized
 # Password will be 20 characters long
@@ -47,6 +51,10 @@ password = (upperLetter1 + upperLetter2 + upperLetter3 + upperLetter4 + upperLet
 
 # Randomizes password string
 password = shuffle(password)
+
+# Makes sure that password does not start with special character to comply with certain organizations with that requirement
+while (password.startswith(spcChar1) == True or password.startswith(spcChar2) == True or password.startswith(spcChar3) == True):
+    shuffle(password)
 
 # Prints the randomized strong password
 print("\nNew Password: " + password + "\n")
